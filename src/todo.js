798fcdb7406b17daa4a42e5480d53814719b3724
Kumaguro3 <lily.sergent@gmail.com>
2022-10-03 19:52:25 +0900

    Created foundations on To do list app

---
+++ src/todo.js
@@ -0,0 +1,71 @@
+//Create QuerySelector to input the text change //
+
+window.addEventListener("load", () => {
+  let form = document.querySelector("#new-task-form");
+  let input = document.querySelector("#new-task-input");
+  let list_el = document.querySelector("#tasks");
+
+  //When submitting new value in the form//
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    let task = input.value;
+
+    // Create new entered task inside our div content //
+    let task_el = document.createElement("div");
+    task_el.classList.add("task");
+
+    let task_content_el = document.createElement("div");
+    task_content_el.classList.add("content");
+
+    task_el.appendChild(task_content_el);
+
+    let task_input_el = document.createElement("input");
+    task_input_el.classList.add("text");
+    task_input_el.type = "text";
+    task_input_el.value = task;
+    task_input_el.setAttribute("readonly", "readonly");
+
+    task_content_el.appendChild(task_input_el);
+
+    let task_actions_el = document.createElement("div");
+    task_actions_el.classList.add("actions");
+
+    //Enable edit of existing task//
+    let task_edit_el = document.createElement("button");
+    task_edit_el.classList.add("edit");
+    task_edit_el.innerText = "Edit";
+
+    //Enable Deletion or existing task//
+    let task_delete_el = document.createElement("button");
+    task_delete_el.classList.add("delete");
+    task_delete_el.innerText = "Delete";
+
+    task_actions_el.appendChild(task_edit_el);
+    task_actions_el.appendChild(task_delete_el);
+
+    task_el.appendChild(task_actions_el);
+
+    list_el.appendChild(task_el);
+
+    input.value = "";
+
+    //Changing input when editing and enable saving or deleting new input inside an existing line//
+
+    task_edit_el.addEventListener("click", (event) => {
+      if (task_edit_el.innerText.toLowerCase() == "edit") {
+        task_edit_el.innerText = "Save";
+        task_input_el.removeAttribute("readonly");
+        task_input_el.focus();
+      } else {
+        task_edit_el.innerText = "Edit";
+        task_input_el.setAttribute("readonly", "readonly");
+      }
+    });
+
+    task_delete_el.addEventListener("click", (event) => {
+      list_el.removeChild(task_el);
+    });
+  });
+});
